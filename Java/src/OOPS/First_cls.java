d27c3a3f9daf023e32e5d94b3e9d4a5b2f5c77d6
vinbond <vinayakkundangar77@gmail.com>
2024-07-08 22:35:19 +0530

    oops

---
+++ Java/src/OOPS/First_cls.java
@@ -0,0 +1,12 @@
+package OOPS;
+
+public class First_cls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Verry Big Wel come Back!");
+		
+
+	}
+
+}
